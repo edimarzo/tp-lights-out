802ded8d7d393354d462b19e44fe4178d597e301
edimarzo <egmdmb@gmail.com>
2017-11-21 20:34:10 -0300

    Se agrega modulo modificado de Niveles. Ahora es una lista de listas. Por otro lado, se incorpora el módulo transformaMapa que toma el mapa compuesto por lista de listas y genera la cuadrícula para mostrar. También se incorpora el modulo transformadorCoordenadas para generar las coordenadas a partir de los datos ingresados por el usuario.

--- modoJuegoPredeterminado.py
+++ modoJuegoPredeterminado.py
@@ -1,11 +1,14 @@
 import niveles
+import transformaMapa
 def modoJuegoPredeterminado():
     print("Bienvenido al modo predeterminado")
 
     for numeroNivel in range (1,6):
 
         print("Nivel {}".format(numeroNivel))
+
         mapaNivel = niveles.mapaNivelPredeterminado(numeroNivel)
+
 
         for linea in mapaNivel:
             print(linea)
